28bc400f6feb7f05a05669afba8ab8f7e5d93031
Lamba <basti.lamberto@gmail.com>
2018-02-09 19:09:08 +0100

    OMMIODIO L'AUTH FUNZIONA NON CI CREDO

--- src/main/kotlin/it/unito/projector/ProjectorApplication.kt
+++ src/main/kotlin/it/unito/projector/ProjectorApplication.kt
@@ -6,15 +6,22 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.core.Ordered
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import java.util.*
 
-//@EnableResourceServer
+
+@EnableResourceServer
 @SpringBootApplication
 class ProjectorApplication {
 
     companion object {
 
-        const val BACKEND_URL = "http://localhost:4200"
+        const val FRONTEND_URL  = "http://localhost:4200"
 
         @JvmStatic
         fun main(args: Array<String>) {
@@ -40,5 +47,19 @@ class ProjectorApplication {
             p.addOwner("Lamberto Basti")
             projectRepo.save(p)
         }
+    }
+
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = Collections.singletonList(FRONTEND_URL)
+        config.allowedMethods = Collections.singletonList("*")
+        config.allowedHeaders = Collections.singletonList("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
     }
 }
--- src/main/kotlin/it/unito/projector/data/repositories/ProjectRepository.kt
+++ src/main/kotlin/it/unito/projector/data/repositories/ProjectRepository.kt
@@ -7,24 +7,28 @@ import it.unito.projector.data.entities.project.Repo
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.web.bind.annotation.CrossOrigin
 
+@EnableResourceServer
 @RepositoryRestResource
-@CrossOrigin(ProjectorApplication.BACKEND_URL)
+@CrossOrigin(ProjectorApplication.FRONTEND_URL)
 interface ProjectRepository : JpaRepository<Project, Long> {
     @Suppress("unused")
     fun findByTitleContainingIgnoreCase(@Param("title") title: String): List<Project>
 }
 
+@EnableResourceServer
 @RepositoryRestResource
-@CrossOrigin(ProjectorApplication.BACKEND_URL)
+@CrossOrigin(ProjectorApplication.FRONTEND_URL)
 interface RepoRepository : JpaRepository<Repo, Long>{
     @Suppress("unused")
     fun findByTitleContainingIgnoreCase(@Param("title") title: String): List<Repo>
 }
 
+@EnableResourceServer
 @RepositoryRestResource
-@CrossOrigin(ProjectorApplication.BACKEND_URL)
+@CrossOrigin(ProjectorApplication.FRONTEND_URL)
 interface KeynoteRepository : JpaRepository<Keynote, Long>{
     @Suppress("unused")
     fun findByTitleContainingIgnoreCase(@Param("title") title: String): List<Keynote>
